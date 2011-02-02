4428562ea0f1ae873b596a71a1b47a74c5288795
Min Huang <mhuang@attinteractive.com>
2011-02-02 11:25:11 -0800

    Upgrade to httpclient 4.1

--- project.clj
+++ project.clj
@@ -2,7 +2,7 @@
   :description "Normalizes and standardizes URLs in a consistent manner."
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
-                 [org.apache.httpcomponents/httpclient "4.1-beta1" :exclusions
+                 [org.apache.httpcomponents/httpclient "4.1" :exclusions
                    [commons-logging
                     commons-codec]]]
   :dev-dependencies
