960565d1a13308e5f490c186e7814f1683e538b6
Min Huang <min.huang@alumni.usc.edu>
2011-02-03 14:28:11 -0800

    Update doc strings

--- test/url_normalizer/test/core.clj
+++ test/url_normalizer/test/core.clj
@@ -17,7 +17,9 @@
 (def
   ^{:doc
     "Tests from RFC3986: Section 5.3.  The keys should resolve against
-    http://a/b/c/d;p?q to the values."}
+    http://a/b/c/d;p?q to the values.
+
+    See <http://www.ietf.org/rfc/rfc3986.txt>"}
   normal-reference-resolution-examples
   (as-uri-map
     {"g:h" "g:h"
@@ -46,7 +48,9 @@
 (def
   ^{:doc
     "Tests from RFC3986 Section 5.4.2.  The keys should resolve against
-    http://a/b/c/d;p?q to the values."}
+    http://a/b/c/d;p?q to the values.
+
+    See <http://www.ietf.org/rfc/rfc3986.txt>"}
   abnormal-reference-resolution-examples
   (as-uri-map
     {"../../../g" "http://a/g"
@@ -67,6 +71,46 @@
      "g?y/../x" "http://a/b/c/g?y/../x"
      "g#s/./x" "http://a/b/c/g#s/./x"
      "g#s/../x" "http://a/b/c/g#s/../x"}))
+
+(def
+  ^{:doc
+    "Pace tests.
+
+    See <http://www.intertwingly.net/wiki/pie/PaceCanonicalIds>"}
+  pace-tests
+  (as-uri-map
+    {"http://:@example.com/" "http://example.com/"
+     "http://@example.com/" "http://example.com/"
+     "http://example.com" "http://example.com/"
+     "HTTP://example.com/" "http://example.com/"
+     "http://EXAMPLE.COM/" "http://example.com/"
+     "http://example.com/%7Ejane" "http://example.com/~jane"
+     "http://example.com/?q=%C3%87" "http://example.com/?q=%C3%87"
+     "http://example.com/?q=%E2%85%A0" "http://example.com/?q=%E2%85%A0"
+     "http://example.com/?q=%5C" "http://example.com/?q=%5C"
+     "http://example.com/a/../a/b" "http://example.com/a/b"
+     "http://example.com/a/./b" "http://example.com/a/b"
+     "http://example.com:80/" "http://example.com/"
+     "http://example.com/" "http://example.com/"
+     "http://example.com/~jane" "http://example.com/~jane"
+     "http://example.com/a/b" "http://example.com/a/b"
+     "http://example.com:8080/" "http://example.com:8080/"
+     "http://user:password@example.com/" "http://user:password@example.com/"}))
+
+(def
+  ^{:doc
+    "Tests from RFC2396.
+
+    See <http://labs.apache.org/webarch/uri/rev-2002/rfc2396bis.html>"}
+  rfc2396bis-tests
+  (as-uri-map
+    {"http://www.ietf.org/rfc/rfc2396.txt" "http://www.ietf.org/rfc/rfc2396.txt"
+     "telnet://192.0.2.16:80/" "telnet://192.0.2.16:80/"
+     "http://127.0.0.1/" "http://127.0.0.1/"
+     "http://127.0.0.1:80/" "http://127.0.0.1/"
+     "http://example.com:081/" "http://example.com:81/"
+     "http://example.com?q=foo" "http://example.com/?q=foo"
+     "http://example.com/?q=foo" "http://example.com/?q=foo"}))
 
 (deftest test-reference-resolution
   (let [base (as-uri "http://a/b/c/d;p?q")]
