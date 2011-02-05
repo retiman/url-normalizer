2b874144a109abdf989cb030617c0fb1e47cf451
Min Huang <mhuang@attinteractive.com>
2011-02-04 16:28:38 -0800

    Bugfix release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject url-normalizer "0.1.2-1"
+(defproject url-normalizer "0.1.3"
   :description "Normalizes and standardizes URLs in a consistent manner."
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
