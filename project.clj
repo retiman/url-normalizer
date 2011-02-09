abd1431585a7e23d2b48281207477d61796a46b5
Min Huang <mhuang@attinteractive.com>
2011-02-09 14:28:55 -0800

    Warn on reflection

--- project.clj
+++ project.clj
@@ -1,5 +1,7 @@
 (defproject url-normalizer "0.1.3"
   :description "Normalizes and standardizes URLs in a consistent manner."
+  :min-lein-version "1.4.2"
+  :warn-on-reflection true
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
                  [org.apache.httpcomponents/httpclient "4.1" :exclusions
