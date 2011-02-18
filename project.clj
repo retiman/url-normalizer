5e84c6d2ee4d7be826da03f2cfc77dfd5538d364
Min Huang <min.huang@alumni.usc.edu>
2011-02-17 20:42:17 -0800

    Apache Abdera has libs for Punycode conversion

--- project.clj
+++ project.clj
@@ -2,13 +2,19 @@
   :description "Normalizes and standardizes URLs in a consistent manner."
   :min-lein-version "1.4.2"
   :warn-on-reflection true
-  :dependencies [[org.clojure/clojure "1.2.0"]
-                 [org.clojure/clojure-contrib "1.2.0"]
-                 [org.apache.httpcomponents/httpclient "4.1" :exclusions
-                   [commons-logging
-                    commons-codec]]]
+  :java-source-path "src"
+  :dependencies
+    [[org.clojure/clojure "1.2.0"]
+     [org.clojure/clojure-contrib "1.2.0"]
+     [org.apache.abdera/abdera-i18n "1.1.1" :exclusions
+       [commons-codec
+        geronimo-activation]]
+     [org.apache.httpcomponents/httpclient "4.1" :exclusions
+       [commons-logging
+        commons-codec]]]
   :dev-dependencies
-    [[autodoc "0.7.1"]
+    [[lein-javac "1.2.1-SNAPSHOT"]
+     [autodoc "0.7.1"]
      [robert/hooke "1.1.0"]
      [swank-clojure "1.2.1"]]
   :test-selectors
