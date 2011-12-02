2ffcbc5580bf81692c2020840790f88507c251b3
Min Huang <min.huang@alumni.usc.edu>
2011-12-01 20:07:48 -0800

    Bump

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject url-normalizer "0.4.1-SNAPSHOT"
+(defproject url-normalizer "0.4.1"
   :description "Normalizes and standardizes URLs in a consistent manner."
   :min-lein-version "1.6.2"
   :warn-on-reflection true
