03bf5e166bf9f308c0ae7528b75945f25eed1c85
Min Huang <min.huang@alumni.usc.edu>
2013-02-08 22:05:29 -0800

    It's a big mystery to me how leiningen does this, but at lease this junk seems to make it happy.

--- project.clj
+++ project.clj
@@ -1,9 +1,13 @@
 (defproject url-normalizer "0.5.3"
   :description "Normalizes and standardizes URLs in a consistent manner."
+  :url "https://github.com/retiman/url-normalizer"
+  :license {:name "Eclipse Public License - v 1.0"
+            :url "http://www.eclipse.org/legal/epl-v10.html"
+            :distribution :repo}
   :min-lein-version "2.0.0"
   :warn-on-reflection true
   :repositories
-    [["clojars" {:sign-releases false}]]
+    [["releases" {:url "https://clojars.org/repo" :sign-releases false}]]
   :dependencies
     [[org.clojure/clojure "1.4.0"]
      [org.flatland/ordered "1.4.0"]
