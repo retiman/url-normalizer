6ae4908ccabf2a7576f22125457a79ca717890f9
Min Huang <min.huang@alumni.usc.edu>
2013-02-08 21:24:55 -0800

    Bump version; 0.5.2 exists

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject url-normalizer "0.5.2"
+(defproject url-normalizer "0.5.3"
   :description "Normalizes and standardizes URLs in a consistent manner."
   :min-lein-version "2.0.0"
   :warn-on-reflection true
